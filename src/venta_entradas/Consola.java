2c396caf8df4927115aad51a5162ce7bde80d3df
Andres Vargas <a.m.vargas1996@gmail.com>
2022-03-27 19:57:18 -0300

    Getter y setters de clases, disposicion de Consola basico

--- src/venta_entradas/Consola.java
+++ src/venta_entradas/Consola.java
@@ -1,10 +1,62 @@
 package venta_entradas;
 
+import java.util.ArrayList;
+
 public class Consola {
+	
+	private ArrayList<Clientes> clientes = new ArrayList<>();
+	private ArrayList<Entradas> entradas = new ArrayList<>();
+	private ArrayList<Ventas> ventas = new ArrayList<>();
+	private ArrayList<Asientos> asientos = new ArrayList<>();
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		menu();
+		inicializar();
+		comprarTicket();
+		verEntradasVendidas();
+		verEntradasDisponibles();
+		verRecaudaciones();
+		
 
 	}
 
+	private static void inicializar() {
+		
+		
+	}
+
+	//MENU
+//////////////////////////////////////////////
+	private static void menu() {
+		// TODO Auto-generated method stub
+		
+	}
+//////////////////////////////////////////////
+	
+	//VER RECAUDACIONES
+///////////////////////////////////////////////
+	private static void verRecaudaciones() {
+		// TODO Auto-generated method stub
+		
+	}
+///////////////////////////////////////////////
+	private static void verEntradasDisponibles() {
+		// TODO Auto-generated method stub
+		
+	}
+	//VER ENTRADAS VENDIDAS
+///////////////////////////////////////////////
+	private static void verEntradasVendidas() {
+		// TODO Auto-generated method stub
+		
+	}
+///////////////////////////////////////////////
+	// COMPRAR TICKET
+///////////////////////////////////////////////
+	private static void comprarTicket() {
+		
+	}
+///////////////////////////////////////////////
 }
