59c06153d451cdd9c14a542532c454c76c1216f3
Rafael <rafael_30_08@hotmail.com>
2022-03-27 20:26:00 -0300

    creacion de Asociacion en clases

--- src/venta_entradas/Ventas.java
+++ src/venta_entradas/Ventas.java
@@ -2,18 +2,42 @@ package venta_entradas;
 
 import java.time.LocalDate;
 
+
+
 public class Ventas {
 	
+	Clientes cliente;
+	Entradas entrada;
 	private LocalDate fechaVenta;
 
-	public Ventas(LocalDate fechaVenta) {
+	public Ventas(LocalDate fechaVenta, Clientes cliente, Entradas entrada) {
 		
 		this.fechaVenta = fechaVenta;
+		this.cliente = cliente;
+		this.entrada = entrada;
 	}
 	
 	public Ventas() {
 		
 	}
+	
+	public Clientes getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Clientes cliente) {
+		this.cliente = cliente;
+	}
+
+	public Entradas getEntrada() {
+		return entrada;
+	}
+
+	public void setEntrada(Entradas entrada) {
+		this.entrada = entrada;
+	}
+
+	
 
 	public LocalDate getFechaVenta() {
 		return fechaVenta;
